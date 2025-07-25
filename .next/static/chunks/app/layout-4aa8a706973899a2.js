1c442ffe503eacdc0205615b0044ad5855ad9212
xadyr-sbi <xadyran@gmail.com>
2025-07-25 10:12:28 +0700

    chore: remove .next from repository

--- .next/static/chunks/app/layout-4aa8a706973899a2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},1989:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,5356,23)),Promise.resolve().then(l.t.bind(l,347,23))},5356:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_e8ce0c"}}},e=>{var s=s=>e(e.s=s);e.O(0,[444,441,684,358],()=>s(1989)),_N_E=e.O()}]);
